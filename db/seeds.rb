292ce64def0b76fe1c7e349c1e5e3426f32a24a3
fherrerao <fherrera0206@gmail.com>
2022-05-30 16:18:41 -0500

    Create migration for greetings, schema and populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.destroy_all
+Greeting.create(message: 'Hello World!')
+Greeting.create(message: 'Hello Rails-React!')
+Greeting.create(message: 'Hello Developer World!')
+Greeting.create(message: 'Hello React-Redux!')
+Greeting.create(message: 'Hello Ruby on Rails!')
