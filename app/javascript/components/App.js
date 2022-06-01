c29461097e97f301df038cec04af3395ee30df1a
fherrerao <fherrera0206@gmail.com>
2022-05-31 20:05:17 -0500

    Add provider to api.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,14 +2,19 @@ import React from "react"
 import PropTypes from "prop-types"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Greeting from "./Greeting";
+import { Provider } from "react-redux";
+import store from "../redux/configureStore";
+
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={ <Greeting message= "Partidazo" /> } />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={ <Greeting /> } />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
