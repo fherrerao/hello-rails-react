140d3321106ec28360312c0cf24f3eb30bc9b59a
fherrerao <fherrera0206@gmail.com>
2022-06-30 21:00:40 -0500

    Remove useless code

--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
 import thunk from "redux-thunk";
 import greetingReducer from "./greetings/greetings";
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
     resources :greetings, only: [:index]
   end 
 
-  get '*page', to: 'greetings#index', constraints: ->(req) do
-    !req.xhr? && req.format.html?
-  end
+  # get '*page', to: 'greetings#index', constraints: ->(req) do
+  #   !req.xhr? && req.format.html?
+  # end
 
   # Defines the root path route ("/")
   root "greetings#index"
